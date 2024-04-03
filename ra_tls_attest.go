02d6789b704847864bafd95eb090469cbf56430a
Pablo Buitrago <pablo@briefboards.com>
2024-04-02 21:37:29 -0300

    qw

--- ra_tls_attest.go
+++ ra_tls_attest.go
@@ -1,7 +1,7 @@
 package main
 
 // #cgo CFLAGS: -I/usr/include
-// #cgo LDFLAGS: -L/lib/x86_64-linux-gnu -ldl -llibra_tls_attest
+// #cgo LDFLAGS: -L/lib/x86_64-linux-gnu -ldl -lra_tls_attest
 // #include <ra_tls.h>
 // #include <stdlib.h>
 import "C"
